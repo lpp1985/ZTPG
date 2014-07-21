c26e08fed6ab879795aa322fe95abd4aae11c830
root <root@Fat.(none)>
2014-07-21 17:04:27 +0800

    four commit

--- configure.py
+++ configure.py
@@ -9,7 +9,6 @@ import os
 config = ConfigParser()
 root_path = os.path.abspath(os.path.split(__file__)[0] ) +'/'
 config.read( root_path+'config.ini'  )
-amos_path = config.get("PATH","AMOSBIN")
 cele_path = config.get( "PATH","CELEDIR"    )
 nucmer = config.get( "PATH","NUCMER" )
 delta_filter = config.get( "PATH","DELTAFILTER" )
