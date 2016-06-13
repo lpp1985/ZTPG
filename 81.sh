2de70f393218a2ca636cffd99d1821a680c92757
Your Name <you@example.com>
2016-06-13 21:21:39 +0800

    123

--- 81.sh
+++ 81.sh
@@ -1,5 +1,5 @@
 git add . -A
 git commit -m 123
-git pull -f Homenas master:81
-git pull -f Roomnas master:81
+#git pull -f Homenas master:81
+#git pull -f Roomnas master:81
 git pull -f USA master:81
